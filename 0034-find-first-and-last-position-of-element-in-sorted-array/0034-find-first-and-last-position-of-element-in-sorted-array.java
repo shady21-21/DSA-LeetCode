7957070b8875522ef5e2d0e0fbcb2f2d05652d1b
Comp <145922247+bd222@users.noreply.github.com>
2024-03-29 10:37:15 +0530

    Time: 0 ms (100.00%), Space: 46 MB (16.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,31 +3,12 @@ class Solution {
         
         
         int[]res = new int[2];
-        res[1] = FistOcc(nums, target);
-        res[0] = LastOcc(nums, target);
+        res[0] = FirstOcc(nums, target);
+        res[1] = LastOcc(nums, target);
         return res;
     }
     
-    static int FistOcc(int[]arr, int target){
-        int n = arr.length;
-        int si = 0;
-        int ei = n-1;
-        int firstOcc = -1;
-        while(si<= ei){
-            int mid = (si+ei)/2;
-            if(arr[mid] == target){
-                firstOcc = mid;
-                si = mid+1;
-            }else if(arr[mid]<target){
-                si = mid+1;
-            }else{
-                ei = mid-1;
-            }
-        }
-        return firstOcc;
-    }
-    
-     static int LastOcc(int[]arr, int target){
+    static int LastOcc(int[]arr, int target){
         int n = arr.length;
         int si = 0;
         int ei = n-1;
@@ -36,7 +17,7 @@ class Solution {
             int mid = (si+ei)/2;
             if(arr[mid] == target){
                 LastOcc = mid;
-                ei = mid-1;
+                si = mid+1;
             }else if(arr[mid]<target){
                 si = mid+1;
             }else{
@@ -44,5 +25,24 @@ class Solution {
             }
         }
         return LastOcc;
+    }
+    
+     static int FirstOcc(int[]arr, int target){
+        int n = arr.length;
+        int si = 0;
+        int ei = n-1;
+        int FirstOcc = -1;
+        while(si<= ei){
+            int mid = (si+ei)/2;
+            if(arr[mid] == target){
+                FirstOcc = mid;
+                ei = mid-1;
+            }else if(arr[mid]<target){
+                si = mid+1;
+            }else{
+                ei = mid-1;
+            }
+        }
+        return FirstOcc;
     }
 }
