4c133af31cd6abb8cd58aedb7d4bbaf7a2391130
21 Savage <145922247+21-21-21-21@users.noreply.github.com>
2024-04-12 09:29:58 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (79.64%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -2,7 +2,30 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         
         //Brute Froce Approach.
-        for(int i = 0;i<arr.length;i++){
+        // return bruteFroce(arr, k);
+        return optimalApproach(arr, k);
+      
+    }
+    
+    //Binary Search Approach
+    int optimalApproach(int[]arr, int k){
+        int low = 0;
+        int high = arr.length-1;
+        while(low<= high){
+            int mid = (low+high)/2;
+            int missingNum = arr[mid] - (mid+1);
+            if(k>missingNum){
+                //my ans on right hand side.
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return low+k;
+    }
+    
+    int bruteFroce(int[]arr, int k){
+          for(int i = 0;i<arr.length;i++){
             if(arr[i]<= k){
                 k++;
             }else{
