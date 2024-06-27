0ad98e9c1b245d4c119b54fb62944b71a2efe13e
21 Savage <145922247+21-21-21-21@users.noreply.github.com>
2024-06-27 13:11:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -32,35 +32,41 @@ class DriverClass {
 /*Complete the function below*/
 
 class Solution {
-   
+    // Function to detect cycle in a directed graph.
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-       int vis[] = new int[V];
-       int pathVis[] = new int[V];
-       for(int i =0 ;i<V;i++){
-           if(vis[i] == 0){
-               if(dfsCheck(i, adj, vis, pathVis) == true){
-                   return true;
-               }
-           }
-       }
-       return false;
-    }
-    
-    boolean dfsCheck(int node,ArrayList<ArrayList<Integer>> adj,int vis[],int pathVis[]){
-        vis[node] = 1;
-        pathVis[node]= 1;
         
-        for(int it: adj.get(node)){
-            if(vis[it] == 0){
-                if(dfsCheck(it, adj, vis, pathVis) == true){
-                    return true;
-                }
-            }else if(pathVis[it] == 1){
-                return true;
+        int[] indeg = new int[V];
+        for(int i = 0;i<V;i++){
+            for(int it: adj.get(i)){
+                indeg[it]++;
             }
         }
         
-        pathVis[node] = 0;
-        return false;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0;i<V;i++){
+            if(indeg[i] == 0){
+                q.add(i);
+            }
+        }
+        
+       int cnt = 0;
+        while(!q.isEmpty()){
+            int currNode = q.peek();
+            q.remove();
+            cnt++;
+            
+            for(int it: adj.get(currNode)){
+                //decrease the indeg.
+                indeg[it]--;
+                if(indeg[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+        
+        if(cnt == V){
+            return false;
+        }
+        return true;
     }
 }
