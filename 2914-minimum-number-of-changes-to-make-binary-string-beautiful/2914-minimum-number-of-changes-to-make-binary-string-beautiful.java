65cdda9fd0c52f4b8ec1a32089243f7e2050fe57
Slim Shady <145922247+shady21-21@users.noreply.github.com>
2024-11-06 00:04:48 +0530

    Time: 1155 ms (35.56%), Space: 45.5 MB (7.03%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -2,7 +2,10 @@ class Solution {
     public int minChanges(String s) {
         int cnt = 0;
         for(int i = 1;i<s.length();i+= 2){
+              System.out.println("i = " + i);
             if(s.charAt(i)!= s.charAt(i-1)){
+                System.out.println("CURR CHAR : - > " + s.charAt(i));
+                System.out.println("PREV CHAR : - > " + s.charAt(i-1));
                 cnt++;
             }
         }
